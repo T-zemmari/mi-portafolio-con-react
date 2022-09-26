439da4bff1a2a5f9b8ded6f6de9339dafde4c87e
guirep <tarek.zemmari@gmail.com>
2022-09-26 19:29:24 +0200

    datos de educacion y experiencia ok

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
-    email: 'sreerag.rajan5@gmail.com',
-    phone: '+91 9663437669',
-    address: '412 Ground Floor, Indraprastha Colony, Sector 30-33, Faridabad, Haryana, 121003',
+    email: 'tarek.zemmari@gmail.com',
+    phone: '+34669217903',
+    address: 'Calle tirant lo blanch 21-6 46013 valencia,España',
 
     sheetAPI: ''
 }
--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,23 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Ramjas College, Delhi University',
-        course: 'B.A. Honours History',
-        startYear: '2015',
-        endYear: '2018'
+        institution: 'Lycee slimane',
+        course: 'Bachiller sciencias eperimentales',
+        startYear: '1998',
+        endYear: '1999'
     },
     {
         id: 2,
-        institution: 'Delhi University',
-        course: 'M.A. History',
-        startYear: '2019',
-        endYear: '2021'
+        institution: 'Insituto Xabec',
+        course: 'Técnico superior en mecatrónica industrial',
+        startYear: '2015',
+        endYear: '2017'
     },
     {
         id: 3,
-        institution: 'Masai School of Coding',
+        institution: 'Geekshubs',
         course: 'Fullstack Web-Development',
-        startYear: '2021',
+        startYear: '2020',
         endYear: 'Present'
     },
 ]
