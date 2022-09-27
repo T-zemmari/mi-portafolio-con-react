a83d7ebb046140a16b1385d8057973909d9275fe
guirep <tarek.zemmari@gmail.com>
2022-09-27 19:26:58 +0200

    ahora tenemos tanto el navbar como el header en vertical flotante

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,19 +4,20 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function CollapsibleExample() {
+  let background='linear-gradient(90deg, rgba(0, 138, 241, 1) 0%, rgba(70, 70, 70, 1) 84%, rgba(0, 95, 255, 1) 100%);'
   return (
-    <Navbar collapseOnSelect expand="lg" bg="" variant="dark" style={{background:'#dc3545'}} fixed="top">
+    <Navbar collapseOnSelect expand="lg" bg="" variant="dark" style={{background:background}} fixed="top">
       <Container>
         <Navbar.Brand href="#home"></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#">Sobre Mi</Nav.Link>
-            <Nav.Link href="#">Skills</Nav.Link>
-            <Nav.Link href="#">Experiencias</Nav.Link>      
+            <Nav.Link href="#about" style={{color:'white'}}>Sobre Mi</Nav.Link>
+            <Nav.Link href="#skills"style={{color:'white'}}>Skills</Nav.Link>
+            <Nav.Link href="#jobs"style={{color:'white'}}>Experiencias</Nav.Link>      
           </Nav>
           <Nav>
-            <Nav.Link href="#">Contacto</Nav.Link>          
+            <Nav.Link href="#contact"style={{color:'white'}}>Contacto</Nav.Link>          
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/views/Home/HomePage.jsx
+++ src/views/Home/HomePage.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect ,useState } from "react";
 import Contact from "../../components/Contact/Contact";
 import Header from "../../components/Header/Header";
 import LandingPage from "../../components/LandingPage/LandingPage";
 import NavBar from "../../components/Navbar/Navbar";
 
+
+
 export default function HomePage() {
+  const [width, setWindowWidth] = useState(0);
+  const updateDimensions = () => {
+    const width = window.innerWidth
+    setWindowWidth(width)
+  }
+
+  useEffect(() => {
+
+    updateDimensions();
+
+    window.addEventListener('resize', updateDimensions);
+    return () =>
+      window.removeEventListener('resize', updateDimensions);
+  }, [])
+
+  const responsive = {
+    showTopNavMenu: width > 1023
+  }
   return (
     <>
+
+      <NavBar showTopNavMenu={responsive.showTopNavMenu} />
       <Header />
-      <LandingPage/>
+      <LandingPage />
       <Contact />
     </>
   );
