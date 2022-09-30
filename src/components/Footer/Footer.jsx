d5919682a53e9fffe65c320d4f497d8ab7cd36bc
Tarek <tarek@tedybusiness.com>
2022-09-30 14:49:09 +0200

    trabajando en el footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+
+{/* <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="assets/css/style.css" rel="stylesheet"> */}
+
+export default function Footer() {
+  return (
+    <>
+      <footer id="footer">
+        <div class="container">
+          <h3>Laura Thomson</h3>
+          <p>
+            Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni
+            eligendi fuga maxime saepe commodi placeat.
+          </p>
+          <div class="social-links">
+            <a href="#" class="twitter">
+              <i class="bx bxl-twitter"></i>
+            </a>
+            <a href="#" class="facebook">
+              <i class="bx bxl-facebook"></i>
+            </a>
+            <a href="#" class="instagram">
+              <i class="bx bxl-instagram"></i>
+            </a>
+            <a href="#" class="google-plus">
+              <i class="bx bxl-skype"></i>
+            </a>
+            <a href="#" class="linkedin">
+              <i class="bx bxl-linkedin"></i>
+            </a>
+          </div>
+          <div class="copyright">
+            &copy; Copyright{" "}
+            <strong>
+              <span>Laura</span>
+            </strong>
+            .Derechos reservados
+          </div>
+          <div class="credits">
+            Designed by <a>Tarek Zemmari Kissani</a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
